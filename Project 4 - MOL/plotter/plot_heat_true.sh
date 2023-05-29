822804f6fe50134c575cc22cd183e05c317716fa
HUANG Wenchong <ebola@zju.edu.cn>
2023-05-29 11:34:47 +0800

    Updated the Fourier solver.

--- Project 4 - MOL/plotter/plot_heat_true.sh
+++ Project 4 - MOL/plotter/plot_heat_true.sh
@@ -1,4 +1,4 @@
-./../trueSol 0
+./../trueSol 0 1e-3
 echo "
 set terminal postscript eps size 5,4
 set xrange [0:1]
@@ -7,7 +7,7 @@ set output '../report/figures/heat-a-0-0.eps'
 plot 'result.txt' w l title ''
 exit
 " | gnuplot
-./../trueSol 0.0025
+./../trueSol 0.0025 1e-3
 echo "
 set terminal postscript eps size 5,4
 set xrange [0:1]
@@ -16,7 +16,7 @@ set output '../report/figures/heat-a-0-1.eps'
 plot 'result.txt' w l title ''
 exit
 " | gnuplot
-./../trueSol 0.005
+./../trueSol 0.005 1e-3
 echo "
 set terminal postscript eps size 5,4
 set xrange [0:1]
@@ -25,7 +25,7 @@ set output '../report/figures/heat-a-0-2.eps'
 plot 'result.txt' w l title ''
 exit
 " | gnuplot
-./../trueSol 0.025
+./../trueSol 0.025 1e-3
 echo "
 set terminal postscript eps size 5,4
 set xrange [0:1]
--- Project 4 - MOL/src/trueSol.cpp
+++ Project 4 - MOL/src/trueSol.cpp
@@ -3,28 +3,67 @@ using namespace std;
 
 const double pi = acos(-1);
 
-double initial(const double &x){
-    if(0.45 <= x && x < 0.5) return 20*(x-0.45);
-    else if(0.5 <= x && x < 0.55) return -20*(x-0.55);
-    else return 0;
-}
+class ForierSolver{
+private:
+    int maxn;
+    vector<double> A;
 
-double trueSol(const double &x, const double &t){
-    double sum = 0;
-    for(int k = 1; k <= 20; k++){
-        double Ak = 40/(k*k*pi*pi) * (-sin(9*k*pi/20) + 2*sin(k*pi/2) - sin(11*k*pi/20));
-        sum += Ak * exp(-k*k*pi*pi*t) * sin(k*pi*x);
+    double F(const double &x) const{
+        return 2 * initial(x) * sin(maxn*pi*x);
     }
-    return sum;
-}
+
+    double squareIntegrate(const double &l, const double &r) const{
+        double mid = (l + r) / 2;
+        return (F(l) + 4*F(mid) + F(r)) * (r - l) / 6;
+    }
+
+    // Simpson自适应积分
+    double integrate(const double &l, const double &r, const double &V, const double &eps) const{
+        double mid = (l + r) / 2;
+        double L = squareIntegrate(l,mid);
+        double R = squareIntegrate(mid,r);
+        if(fabs(L+R-V) <= 15*eps) return L + R + (L+R-V) / 15.0;
+        return integrate(l,mid,L,eps) + integrate(mid,r,R,eps);
+    }
+
+    double integrate(const double &l, const double &r, const double &eps) const{
+        return integrate(l, r, squareIntegrate(l,r), max(1e-4*eps, 1e-15));
+    }
+
+public:
+    ForierSolver(const double &eps){
+        maxn = 0;
+        do{
+            ++maxn;
+            A.push_back(integrate(0, 1, eps));
+        } while(maxn < 2 || fabs(A[maxn-1]) > eps || fabs(A[maxn-2]) > eps);
+        cout << maxn << " items has been retained in the Fourier series." << endl;
+    }
+
+    double initial(const double &x) const{
+        if(0.45 <= x && x < 0.5) return 20*(x-0.45);
+        else if(0.5 <= x && x < 0.55) return -20*(x-0.55);
+        else return 0;
+    }
+
+    double operator () (const double &x, const double &t) const{
+        double sum = 0;
+        for(int k = 1; k <= maxn; k++){
+            sum += A[k-1] * exp(-k*k*pi*pi*t) * sin(k*pi*x);
+        }
+        return sum;
+    }
+};
 
 int main(int argc, char* argv[]){
-    double t = stod(argv[1]);
+    double t = stod(argv[1]), eps = stod(argv[2]);
     ofstream fout("result.txt");
+    ForierSolver truesol(eps);
     for(int i = 0; i <= 1000; i++){
-        double val = t ? trueSol(0.001*i, t) : initial(0.001*i);
+        double val = t ? truesol(0.001*i, t) : truesol.initial(0.001*i);
         fout << 0.001*i << " " << val << "\n";
     }
     fout.close();
+    cout << "The result at t=" << t << " has been saved to result.txt." << endl;
     return 0;
 }
