f6551b586fd25ddc5b8895ddcebeb80ad4557ea4
HUANG Wenchong <ebola@zju.edu.cn>
2023-06-07 21:16:06 +0800

    update gifgen

--- Project 3 - IVPs and MOL/MOL/gengif.m
+++ Project 3 - IVPs and MOL/MOL/gengif.m
@@ -1,19 +1,18 @@
-B = load("result.txt");
-t=-2:0.05:25;
+B = load("result-dense.txt");
+t=0.01:0.01:0.99;
 m = size(B,2);
 f=figure;
 for j=1:size(B,1)
-    %% 将y=2*cos(x+n(idx))的图像绘制出来
     plot(t, B(j, 2:m));
-    xlim([-2,25]);
-    ylim([-0.4,1.1]);
+    xlim([0,1]);
+    ylim([-0.6,1.6]);
     drawnow
     %% 将所有的图像显示在同一个图窗中
     frame=getframe(f);
     i{j}=frame2im(frame);
     [A,map]=rgb2ind(i{j},256);
     %% 将上面合成后的图像导出为gif文件
-    filename='advection7.gif';
+    filename='heat.gif';
     if j==1
         imwrite(A,map,filename,'gif','LoopCount',Inf,'DelayTime',0.04);
     else
